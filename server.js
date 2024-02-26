81b6a34268bc8fe79844c65463b5e93c747268ac
Débora Fortunato Dias <debfdias@gmail.com>
2024-02-26 17:32:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const connectDB = require("./config/db");
+//const connectDB = require("./config/db");
 const userRoutes = require("./routes/users");
 const errorHandler = require("./middlewares/error");
 
 
 // Connect to DB
-connectDB();
+//connectDB();
 
 // Express App
 const app = express();
