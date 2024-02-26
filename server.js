91b5124e8bfa66e82c3255915ce22c0ac7484877
Débora Fortunato Dias <debfdias@gmail.com>
2024-02-26 17:53:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,12 @@ app.use("/api/products", (req, res) => {
   })
 });
 
+app.use("/api/hey", (req, res) => {
+  return res.status(200).json({
+    message: 'Hey ho! Lets go!'
+  })
+});
+
 app.use(errorHandler);
 
 const server = app.listen(port, () =>
